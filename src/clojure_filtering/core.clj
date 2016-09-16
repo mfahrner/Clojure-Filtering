89e3f2500f7256c56bf817d1c34a3f5b6664867a
michael fahrner <Mike Fahrner>
2016-09-15 22:28:28 -0400

    almost final

--- src/clojure_filtering/core.clj
+++ src/clojure_filtering/core.clj
@@ -1,6 +1,41 @@
-(ns clojure-filtering.core)
+(ns clojure-filtering.core
+  (:require [clojure.data.json :as json][clojure.string :as str])
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+
+(defn -main []
+  (println "Type a category:")
+  (let [purchases (slurp "purchases.csv")
+        ;spliting string into vector of strings by line.
+        ; uses "imported" clojure.string which is assigned to "alias" str
+        purchases (str/split-lines purchases)
+        ;splits each line into a vector of strings based on commas
+        purchases (map (fn [line]
+                      (str/split line #","))
+                    purchases)
+        ; assigns local variable header to the first vector of people
+        ; will now not include top line unless header is included in return statement
+        header (first purchases)
+        ; rebinds purchases to be everything but the first line
+        purchases (rest purchases)
+        ; rebinds purchases to a map of hashmaps
+        ; each line is turned into a hashmap
+        ; zipmap mixes the two collections header and line together
+        ; each hashmap key comes from header and value from line
+        purchases (map (fn [line]
+                      (zipmap header line))
+                    purchases)
+        ; assigns var category to line read in after prompt
+        category (read-line)
+        ; filters by line
+        ; evaluates statement between "category" value and what user passes in from prompt
+        ; rebinds purchases to map of hashmaps that return true
+        purchases (filter (fn [line]
+                         (= (get line "category") category))
+                       purchases)
+        ; converts text file to json
+        ; uses "import" which is declared in ns :require
+        file-text (json/write-str purchases)]
+    ; writes file
+    (spit "filtered_purchases.json" file-text)))
